37b79c32c102b284eca7cfcd498444798f503b47
bmael <bn.mael@gmail.com>
2013-11-25 01:23:17 +0100

    Add null test

--- src/Alarm/fr/nantes/univ/alma/alarm/impl/TimerThread.java
+++ src/Alarm/fr/nantes/univ/alma/alarm/impl/TimerThread.java
@@ -14,23 +14,25 @@ import fr.nantes.univ.alma.common.remote.IAlarm;
  *
  */
 public class TimerThread extends Thread {
-	
+
 	private IAlarm alarm;
 	private boolean stop;
-	
+
 	public TimerThread(IAlarm alarm){
 		this.alarm = alarm;
 		this.stop = false;
 	}
-	
+
 	public void run(){
 		while(!stop){
 			try {
 				Date current = new Date();
 				SimpleDateFormat format = new SimpleDateFormat("dd MMMM yyyy HH:mm");
-				if(format.format(this.alarm.getRingingDate()).equals(format.format(current))){
-					System.out.println("DRINNNNNNNNNNNNNNGGGGGGGGGGGGGGGGGG!!!!!!!!!!!!!!");
-					this.alarm.ringing();
+				if(this.alarm.getRingingDate() != null){
+					if(format.format(this.alarm.getRingingDate()).equals(format.format(current))){
+						System.out.println("DRINNNNNNNNNNNNNNGGGGGGGGGGGGGGGGGG!!!!!!!!!!!!!!");
+						this.alarm.ringing();
+					}
 				}
 			} catch (RemoteException e) {
 				System.err.println("Unable to get the ringing date... Enjoy your alarm never ring!");
@@ -44,9 +46,9 @@ public class TimerThread extends Thread {
 			}
 		}
 	}
-	
+
 	public void stopTimer(){
 		this.stop = true;
 	}
-	
+
 }
